b2e4d4db78904eb4e4de623f7d26a4b3453a2cca
maxwellwachira <maxwellwachira67@gmail.com>
2023-02-05 22:35:58 +0300

    updated sandbox and prod urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = process.env.PESAPAL_ENV as string;
+const port = process.env.PORT;
+
 export const urls = {
-    clientUrl: 'https://ategoschool.com'
+    clientUrl: env === "sandbox" ? "http://localhost:3000" : "https://ategoschool.com",
+    pesapalBaseUrl: env === "sandbox" ? "https://cybqa.pesapal.com/pesapalv3" : "https://pay.pesapal.com/v3",
+    baseUrl: `http://localhost:${port}`
 }
