c9bdbe4339914059fd4bf8674d3755e678d352d1
maxwellwachira <maxwellwachira67@gmail.com>
2023-02-13 22:29:21 +0300

    added role based authorization middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,13 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
+import { findUserById } from "../appUsers/users/userService";
 
 dotenv.config();
 
 const SECRET_KEY = process.env.SECRET_KEY as Secret;
 
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.headers.authorization?.split(" ")[1];
         console.log(token)
@@ -15,8 +16,13 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
 
         const decodeData = jwt.verify(token, SECRET_KEY) as JwtPayload;
 
+        const user = await findUserById(decodeData?.id);
+        if (!user) return res.status(404).json({message: `user does not exists`}); 
+        const userObject = user.toJSON();
+        
         res.locals.userId = decodeData?.id;
-
+        res.locals.role = userObject.role;
+        
         return next();
 
     } catch (error: any) {
